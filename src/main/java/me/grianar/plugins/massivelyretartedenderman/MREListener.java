1d37c4b4016f5f12753968b57dd02e07c272e283
Connor Wilkerson <wilkersonconnor@ymail.com>
2012-03-04 13:02:36 -0600

    Commit

--- src/main/java/me/grianar/plugins/massivelyretartedenderman/MREListener.java
+++ src/main/java/me/grianar/plugins/massivelyretartedenderman/MREListener.java
@@ -1,6 +1,7 @@
 package me.grianar.plugins.massivelyretartedenderman;
 
 import org.bukkit.Location;
+import org.bukkit.block.BlockState;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -15,7 +16,23 @@ public class MREListener implements Listener {
         if (e == EntityType.ENDERMAN) {
             Entity enderman = event.getEntity();
             Location loc = enderman.getLocation();
+            BlockState[][][] nearbyBlocks = new BlockState[16][16][16];
+            for (int x = 0; x < 16; x++) {
+                for (int y = 0; y < 16; y++) {
+                    for (int z = 0; z < 16; z++) {
+                        nearbyBlocks[x][y][z] = loc.getBlock().getRelative(x - 8, y - 8, z - 8).getState();
+                    }
+                }
+            }
             event.getEntity().getWorld().createExplosion(loc, 4F);
+
+            for (int x = 0; x < 16; x++) {
+                for (int y = 0; y < 16; y++) {
+                    for (int z = 0; z < 16; z++) {
+                        nearbyBlocks[x][y][z].update(true);
+                    }
+                }
+            }
         }
     }
 }
